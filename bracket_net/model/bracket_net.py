ac638916f0ce94b5782ebafdf3e44a03217c867d
bottlenome <bottlenome@gmail.com>
2024-02-05 00:27:42 +0900

    Add mutihead functionality

--- bracket_net/model/bracket_net.py
+++ bracket_net/model/bracket_net.py
@@ -48,128 +48,120 @@ class BracketNet(nn.Module):
 
 
 class LieFuncBase():
-    def __init__(self, bracket):
+    def __init__(self, bracket, d_model, n_head, dim):
         self.bracket = bracket
+        self.d_model = d_model
+        self.n_head = n_head
+        self.dim = dim
+        self.layer_norm = nn.LayerNorm(d_model)
 
     @abc.abstractmethod
-    def lie_func(self, i, src, context):
+    def lie_func(self, c, x, head_id):
+        # c, x: [batch, 1, dim]
         raise NotImplementedError()
 
-    def __call__(self, i, src, context):
-        return self.lie_func(i, src, context)
+    # src: [batch, d_model]
+    def __call__(self, src, context):
+        # batch, d_model to batch, n_head, dim
+        srcs = src.view(-1, self.n_head, self.dim)
+        y = torch.zeros_like(srcs)
+        contexts = context.view(-1, self.n_head, self.dim)
+        c = torch.zeros_like(contexts)
+
+        for head_id in range(self.n_head):
+            c[:, head_id], y[:, head_id] = self.lie_func(
+                    srcs[:, head_id],
+                    contexts[:, head_id],
+                    head_id)
+
+        c = c.view(-1, self.d_model)
+        c = self.layer_norm(c)
+        y = y.view(-1, self.d_model)
+        y = self.layer_norm(y)
+        return c, y
 
     def initialize_context(self, src):
         return torch.zeros_like(src[0])
 
 
 class LieFuncBasic(LieFuncBase):
-    def lie_func(self, i, src, context):
-        if i == 0:
-            x = torch.zeros_like(src[0])
-            y = src[0]
-        else:
-            x = src[i - 1]
-            y = src[i]
-        context = context + x + self.bracket(x, y)
-        return context, context
+    def lie_func(self, c, x, head_id):
+        context = x
+        y = self.bracket(c, x, head_id)
+        return context, y
 
 
 class LieFuncWithoutContext(LieFuncBase):
-    def lie_func(self, i, src, context):
-        if i == 0:
-            x = torch.zeros_like(src[0])
-            y = src[0]
-        else:
-            x = src[i - 1]
-            y = src[i]
-        return context, y + self.bracket(x, y)
+    def lie_func(self, c, x, head_id):
+        context = x
+        y = x + self.bracket(c, x, head_id)
+        return context, y
 
 
 class LieFuncContextForget(LieFuncBase):
-    def __init__(self, bracket):
-        super().__init__(bracket)
+    def __init__(self, bracket, d_model, n_head, dim):
+        super().__init__(bracket, d_model, n_head, dim)
         self.alpha = nn.Parameter(torch.tensor(0.9))
 
-    def lie_func(self, i, src, context):
-        if i == 0:
-            x = torch.zeros_like(src[0])
-            y = src[0]
-        else:
-            x = src[i - 1]
-            y = src[i]
-        context = self.alpha * context + self.bracket(x, y)
-        r = y + context
-        return context, r
+    def lie_func(self, c, x, head_id):
+        context = self.alpha * c + self.bracket(c, x, head_id)
+        y = x + c
+        return context, y
 
 
 class LieFuncContextForgetMix(LieFuncBase):
-    def __init__(self, bracket):
-        super().__init__(bracket)
+    def __init__(self, bracket, d_model, n_head, dim):
+        super().__init__(bracket, d_model, n_head, dim)
         self.alpha = nn.Parameter(torch.tensor(0.9))
         self.beta = nn.Parameter(torch.tensor(0.9))
 
-    def lie_func(self, i, src, context):
-        if i == 0:
-            x = torch.zeros_like(src[0])
-            y = src[0]
-        else:
-            x = src[i - 1]
-            y = src[i]
-        context = self.alpha * context + self.beta * y + self.bracket(x, y)
-        r = context
-        return context, r
+    def lie_func(self, c, x, head_id):
+        context = self.alpha * c + self.beta * x + self.bracket(c, x, head_id)
+        y = context
+        return context, y
 
 
 class LieFuncBracketRule(LieFuncBase):
-    def __init__(self, bracket):
-        super().__init__(bracket)
+    def __init__(self, bracket, d_model, n_head, dim):
+        super().__init__(bracket, d_model, n_head, dim)
         self.alpha = nn.Parameter(torch.tensor(0.5))
 
-    def lie_func(self, i, src, context):
-        if i == 0:
-            x = torch.ones_like(src[0])
-            y = src[0]
-        else:
-            x = src[i - 1]
-            y = src[i]
-        context = self.bracket(context.clone().detach(), self.bracket(x, y))
-        r = self.alpha * y + (1 - self.alpha) * context
-        return context, r
+    def lie_func(self, c, x, head_id):
+        context = self.bracket(
+                c.clone().detach(), self.bracket(c, x, head_id), head_id)
+        y = self.alpha * x + (1 - self.alpha) * context
+        return context, y
 
-    def initialize_context(src):
+    def initialize_context(self, src):
         return torch.ones_like(src[0])
 
 
 class LieFuncWithoutContextBracket(LieFuncBase):
-    def __init__(self, bracket):
-        super().__init__(bracket)
+    def __init__(self, bracket, d_model, n_head, dim):
+        super().__init__(bracket, d_model, n_head, dim)
         self.alpha = nn.Parameter(torch.tensor(0.5))
 
-    def lie_func(self, i, src, context):
-        if i == 0:
-            x = torch.zeros_like(src[0])
-            y = src[0]
-        else:
-            x = src[i - 1]
-            y = src[i]
-        return context, self.alpha * y + (1 - self.alpha) * self.bracket(x, y)
+    def lie_func(self, c, x, head_id):
+        c = x
+        return c, (self.alpha * x +
+                   (1 - self.alpha) * self.bracket(c, x, head_id))
 
 
 class LieFuncVectorCondition(LieFuncBase):
-    def lie_func(self, i, src, context):
-        x = src[0]
-        t = src[-1]
-        v = self.bracket(x, t)
-        if i == 0:
-            ret = x + v
-        else:
-            ret = 2 * x + 2 * v - context
-        context = x + 2 * v
-        return context, ret
+    def lie_func(self, c, x, head_id):
+        context = x
+        y = x
+        v = self.bracket(c, x, head_id)
+        # c + v == x : bracket_product becomes direction vector caclucation
+        # c = x[i]
+        # v_2 = self.bracket(x[i], x[i+2])
+        # 2*v = v_2
+        zero_condition = c + v - x
+        return context, y + zero_condition
 
 
 class LieFuncFactory():
-    def __init__(self, bracket):
+    def __init__(self, bracket, d_model, n_head, dim):
         self.map = {"base": LieFuncBasic,
                     "1_without_context": LieFuncWithoutContext,
                     "2_context_forget": LieFuncContextForget,
@@ -178,10 +170,14 @@ class LieFuncFactory():
                     "5_without_context": LieFuncWithoutContextBracket,
                     "6_vector_condition": LieFuncVectorCondition}
         self.bracket = bracket
+        self.d_model = d_model
+        self.n_head = n_head
+        self.dim = dim
 
     def get(self, mode):
         try:
-            return self.map[mode](self.bracket)
+            return self.map[mode](self.bracket, self.d_model,
+                                  self.n_head, self.dim)
         except KeyError:
             raise NotImplementedError(f"mode {mode} is not implemented")
 
@@ -190,19 +186,20 @@ class LieNet(nn.Module):
     def __init__(self, d_model=128, n_head=4, mode="base"):
         super().__init__()
         self.activate = nn.GELU()
-        self.bracket_product = nn.Linear(d_model*2, d_model)
 
         assert(d_model % n_head == 0)
         dim = int(d_model / n_head)
+        self.bracket_products = []
+        for i in range(n_head):
+            self.bracket_products.append(nn.Linear(dim*2, dim))
 
-        def bracket(a, b):
+        def bracket(a, b, i):
             return self.activate(
-                    self.bracket_product(torch.cat([a, b], dim=1)))
+                    self.bracket_products[i](torch.cat([a, b], dim=1)))
 
         self.bracket = bracket
-        self.lie_func = LieFuncFactory(bracket).get(mode)
+        self.lie_func = LieFuncFactory(bracket, d_model, n_head, dim).get(mode)
 
-        self.layer_norm = nn.LayerNorm(d_model)
         self.d_model = d_model
         self.n_head = n_head
         self.dim = dim
@@ -211,7 +208,8 @@ class LieNet(nn.Module):
         context = self.lie_func.initialize_context(src)
         ret = []
         for i in range(src.shape[0]):
-            context, r = self.lie_func(i, src, context)
+            context, r = self.lie_func(src[i], context)
+            """
             if i >= 2:
                 a = src[i - 2]
                 b = src[i - 1]
@@ -220,6 +218,7 @@ class LieNet(nn.Module):
                                    self.bracket(b, self.bracket(c, a)) +
                                    self.bracket(c, self.bracket(a, b)))
                 r += jacobi_identity
+            """
             ret.append(r.clone())
         ret = torch.stack(ret)
         return ret
@@ -228,8 +227,20 @@ class LieNet(nn.Module):
 if __name__ == '__main__':
     def fake(a, b):
         return None
-    Func = LieFuncFactory(fake).get("base")
+    Func = LieFuncFactory(fake, 128, 4, 32).get("base")
     model = BracketNet()
     model = LieNet()
-    src = torch.ones((10, 3, 128))
+    src = torch.ones((3, 10, 128))
+    model(src)
+    model = LieNet(mode="1_without_context")
+    model(src)
+    model = LieNet(mode="2_context_forget")
+    model(src)
+    model = LieNet(mode="3_context_forget")
+    model(src)
+    model = LieNet(mode="4_bracket_rule")
+    model(src)
+    model = LieNet(mode="5_without_context")
+    model(src)
+    model = LieNet(mode="6_vector_condition")
     model(src)
