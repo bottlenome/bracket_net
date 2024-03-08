7b006a781a003cb83dd46f7c655adf231eac9c8b
bottlenome <bottlenome@gmail.com>
2024-03-08 17:01:26 +0900

    Add BracketNet model and divide encoder part to module

--- bracket_net/model/bracket_net.py
+++ bracket_net/model/bracket_net.py
@@ -3,11 +3,9 @@ import torch.nn as nn
 from lie_func import LieFuncFactory
 
 
-class BracketNet(nn.Module):
+class BracketFunc(nn.Module):
     def __init__(self, d_model=128, n_head=4, mode="base"):
         super().__init__()
-        self.activate = nn.GELU()
-
         assert(d_model % n_head == 0)
         dim = int(d_model / n_head)
         self.bracket_products = []
@@ -17,47 +15,79 @@ class BracketNet(nn.Module):
         def bracket(a, b, i):
             return self.activate(
                     self.bracket_products[i](torch.cat([a, b], dim=1)))
-
         self.bracket = bracket
         self.lie_func = LieFuncFactory(bracket, d_model, n_head, dim).get(mode)
 
         self.d_model = d_model
         self.n_head = n_head
         self.dim = dim
+        self.activate = nn.GELU()
+
 
     def forward(self, src):
         context = self.lie_func.initialize_context(src)
         ret = []
         for i in range(src.shape[0]):
             context, r = self.lie_func(src[i], context)
-            """
-            if i >= 2:
-                a = src[i - 2]
-                b = src[i - 1]
-                c = src[i]
-                jacobi_identity = (self.bracket(a, self.bracket(b, c)) +
-                                   self.bracket(b, self.bracket(c, a)) +
-                                   self.bracket(c, self.bracket(a, b)))
-                r += jacobi_identity
-            """
             ret.append(r.clone())
         ret = torch.stack(ret)
         return ret
 
 
+class BracketNet(nn.Module):
+    def __init__(self, d_vocab, pos_encodr,
+                 d_model=128, n_head=4,
+                 num_layers=6, dropout=0.0,
+                 embed=None, mode="base"):
+        super().__init__()
+        self.d_vocab = d_vocab
+        self.d_model = d_vocab
+        self.n_head = n_head
+        self.dim = int(d_vocab / n_head)
+
+        if embed is None:
+            self.embed = torch.nn.Embedding(d_vocab + 1, d_model)
+        else:
+            self.embed = embed
+        self.pos_encoder = pos_encodr(d_model, dropout)
+        self.bracket_funcs = nn.Sequential()
+        for i in range(num_layers):
+            self.bracket_funcs.add_module(
+                f"bracket_func{i}", BracketFunc(d_model, n_head, mode))
+        self.unembed = torch.nn.Linear(d_model, d_vocab)
+    
+    def forward(self, src: torch.Tensor) -> torch.Tensor:
+        src = self.embed(src)
+        src = self.pos_encoder(src)
+        out = self.bracket_funcs(src)
+        out = self.unembed(out)
+        return out  
+
+
 if __name__ == '__main__':
-    model = BracketNet()
-    src = torch.ones((3, 10, 128))
-    model(src)
-    model = BracketNet(mode="1_without_context")
-    model(src)
-    model = BracketNet(mode="2_context_forget")
-    model(src)
-    model = BracketNet(mode="3_context_forget")
-    model(src)
-    model = BracketNet(mode="4_bracket_rule")
-    model(src)
-    model = BracketNet(mode="5_without_context")
-    model(src)
-    model = BracketNet(mode="6_vector_condition")
-    model(src)
+    # batch, map
+    data = torch.ones(10, 32*32, dtype=torch.int64)
+    data = data.permute(1, 0)
+    from encoder import PostionalEncodingFactory
+    encoding = PostionalEncodingFactory("1d", max_len=32*32)
+    model = BracketNet(113, encoding)
+    out = model(data)
+    print(out.shape)
+    model = BracketNet(113, encoding, mode="1_without_context")
+    out = model(data)
+    print(out.shape)
+    model = BracketNet(113, encoding, mode="2_context_forget")
+    out = model(data)
+    print(out.shape)
+    model = BracketNet(113, encoding, mode="3_context_forget")
+    out = model(data)
+    print(out.shape)
+    model = BracketNet(113, encoding, mode="4_bracket_rule")
+    out = model(data)
+    print(out.shape)
+    model = BracketNet(113, encoding, mode="5_without_context")
+    out = model(data)
+    print(out.shape)
+    model = BracketNet(113, encoding, mode="6_vector_condition")
+    out = model(data)
+    print(out.shape)
--- bracket_net/model/gpt.py
+++ bracket_net/model/gpt.py
@@ -1,81 +1,6 @@
 import torch
 import torch.nn as nn
-import math
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
-
-
-class PositionalEncoding(nn.Module):
-    def __init__(self, d_model: int, dropout: float = 0.0, max_len: int = 10):
-        super().__init__()
-        self.dropout = nn.Dropout(p=dropout)
-
-        position = torch.arange(max_len).unsqueeze(1)
-        div_term = torch.exp(
-                torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
-        pe = torch.zeros(max_len, 1, d_model)
-        pe[:, 0, 0::2] = torch.sin(position * div_term)
-        pe[:, 0, 1::2] = torch.cos(position * div_term)
-        self.register_buffer('pe', pe)
-
-    def forward(self, x):
-        x = x + self.pe[:x.size(0)]
-        return self.dropout(x)
-
-
-class PositionalEncoding2D(nn.Module):
-    def __init__(self, d_model: int, height: int, width: int, dropout=0.1):
-        super(PositionalEncoding2D, self).__init__()
-        self.dropout = nn.Dropout(p=dropout)
-
-        # Create a 2D position encoding
-        pe = torch.zeros(height, width, d_model)
-        # Compute the positional encodings once in log space.
-        y_position = torch.arange(0, height).unsqueeze(1)
-        x_position = torch.arange(0, width).unsqueeze(1)
-
-        div_term = torch.exp(
-            torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model))
-        pe[:, :, 0::2] = torch.sin(
-            x_position * div_term) + torch.sin(y_position * div_term)
-        pe[:, :, 1::2] = torch.cos(
-            x_position * div_term) + torch.cos(y_position * div_term)
-        # Shape: [1, d_model, height, width]
-        pe = pe.unsqueeze(0).permute(0, 3, 1, 2)
-        self.register_buffer('pe', pe)
-        self.d_model = d_model
-
-    def forward(self, x):
-        x = x + self.pe[:, :, :x.size(2), :x.size(3)]
-        # batch, c, h, w -> batch, c, seq
-        x = x.view(x.shape[0], self.d_model, -1)
-        x = self.dropout(x)
-        # seq, batch, d_model
-        return x.permute(2, 0, 1)
-
-
-class PostionalEncodingFactory():
-    def __init__(self, model_type, **kwargs):
-        if model_type == "1d":
-            self.model = PositionalEncoding
-        elif model_type == "2d":
-            self.model = PositionalEncoding2D
-        else:
-            print(f"invalid model_type:{model_type}")
-            assert(False)
-        self.model_type = model_type
-        self.kwargs = kwargs
-
-    def __call__(self, d_model, dropout):
-        if self.model_type == "1d":
-            return self.model(d_model=d_model,
-                              max_len=self.kwargs["max_len"],
-                              dropout=dropout)
-        elif self.model_type == "2d":
-            return self.model(d_model=d_model,
-                              height=self.kwargs["height"],
-                              width=self.kwargs["width"],
-                              dropout=dropout)
-        assert(False)
 
 
 class GPT(nn.Module):
@@ -105,8 +30,9 @@ class GPT(nn.Module):
 
 
 if __name__ == '__main__':
+    from encoder import PostionalEncodingFactory
     encoding = PostionalEncodingFactory("1d", max_len=32*32)
-    model = GPT(113, encoding, 32* 32)
+    model = GPT(113, encoding)
     # batch, map
     data = torch.zeros(10, 32 * 32, dtype=torch.int64)
     # map, batch
