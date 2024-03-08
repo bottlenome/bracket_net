72ee447e38993f19214b79789ccb18875db92f2e
bottlenome <bottlenome@gmail.com>
2024-03-08 20:04:26 +0900

    Fix gpt-naive

--- bracket_net/domain/planning/gpt.py
+++ bracket_net/domain/planning/gpt.py
@@ -3,42 +3,13 @@ from ...model import gpt, encoder
 import pytorch_lightning as L
 import torch
 import torch.nn as nn
-from .util import CommonModule
+from .util import CommonModule, NaiveBase
 
 
-class Naive(CommonModule):
+class Naive(NaiveBase):
     def __init__(self, config):
-        super().__init__(config)
-        self.d_vocab = config.gpt.d_vocab
-        self.d_model = config.gpt.d_model
-        self.model = gpt.GPT(self.d_vocab,
-                             encoder.PostionalEncodingFactory(
-                                 "1d", max_len=32*32*4+4+1),
-                             d_model=self.d_model,
-                             nhead=config.gpt.nhead,
-                             num_layers=config.gpt.num_layers,
-                             dropout=config.gpt.dropout)
+        super().__init__(config, gpt.GPT)
 
-    def forward(self, map_designs, start_maps, goal_maps, out_trajs):
-        # batch, 1, 32, 32 -> batch, 32 * 32
-        start_maps = start_maps.view(start_maps.size(0), -1)
-        goal_maps = goal_maps.view(goal_maps.size(0), -1)
-        map_designs = map_designs.view(map_designs.size(0), -1)
-        # concat problem_start, start_maps, goal_maps, map_designs,
-        #        estimate_start, out_trajs, estimate_end
-        src = torch.cat([self.problem_start, start_maps, goal_maps, map_designs,
-                        self.estimate_start,
-                        out_trajs.view(out_trajs.size(0), -1),
-                        self.estimate_end], dim=1)
-        # float to int
-        src = src.to(torch.int64)
-        # batch, seq -> seq, batch
-        src = src.permute(1, 0)
-        # seq, batch -> seq, batch, d_vocab
-        out = self.model(src)
-        # seq, batch, d_vocab -> batch, d_vocab, seq
-        out = out.permute(1, 2, 0)
-        return out
  
 class NNAstarLike(CommonModule):
     def __init__(self, config):
@@ -54,7 +25,7 @@ class NNAstarLike(CommonModule):
                              encoder.PostionalEncodingFactory(
                                  "2d", height=32, width=32),
                              d_model=d_model,
-                             nhead=config.gpt.nhead,
+                             n_head=config.gpt.n_head,
                              num_layers=config.gpt.num_layers,
                              dropout=config.gpt.dropout,
                              embed=self.encode)
