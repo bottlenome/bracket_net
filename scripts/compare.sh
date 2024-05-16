2c9fc5425fb2cf2801b5af88d2a50576924e8e7d
bottlenome <bottlenome@gmail.com>
2024-05-16 09:07:06 +0900

    Update compare script

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -1,17 +1,26 @@
 #!/bin/bash -eu
-experiment_name="diagonal_0"
-branch_name="main"
+# experiment_name="diagonal_0"
+experiment_names=("goal_estimation_from_state"
+                  "entropy_loss"
+                  )
+# branch_name="diagonal_1"
+# branch_name="main"
 num_epochs=50
 
-git checkout $branch_name
-for i in {1..8}
+for branch_name in "${experiment_names[@]}"
 do
-    echo "Running experiment $i"
+experiment_name=$branch_name
+
+git checkout $branch_name
+for i in {1..2}
+do
+    echo "Running experiment ${experiment_name} $i"
     cmd="python -m scripts.train model=bracket-naive experiment_name=${experiment_name}_${i} params.batch_size=10 gpt.num_layers=3 gpt.n_head=1 model.mode=11_beam_search_optimized gpt.d_model=128 data.magnification=1 params.num_epochs=${num_epochs} -m"
     bash -c "$cmd"
-    sleep 10
     # break if sigint received
     if [ $? -eq 130 ]; then
         break
     fi
+done
+
 done
