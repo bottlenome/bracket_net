65f784cd7e079c56c2357ff00f069e4c9098efe8
bottlenome <bottlenome@gmail.com>
2024-06-07 09:36:14 +0900

    Fix for github actions

--- docker_run.sh
+++ docker_run.sh
@@ -1,14 +1,29 @@
 #!/bin/bash -eu
 
-EXEC_PATH=`pwd`
 COMMAND=${1:-bash}
+GITHUB_ACTIONS=${GITHUB_ACTIONS:-false}
 
-docker run -it --gpus all \
-               -w ${EXEC_PATH} \
-               --shm-size=2gb \
-               -h bn_docker \
-               -v /etc/group:/etc/group:ro \
-               -v /etc/passwd:/etc/passwd:ro \
-               -v /home/$USER/:/home/$USER \
-               -v /mnt:/mnt \
-               -u $(id -u $USER):$(id -g $USER) bottlenome/bracket_net:latest ${COMMAND}
+# Determine the execution environment
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+  EXEC_PATH=${GITHUB_WORKSPACE}
+  USER_DIR=${GITHUB_WORKSPACE}
+  DOCKER_USER=""
+  DOCKER_OPTS=""
+else
+  EXEC_PATH=`pwd`
+  USER_DIR="/home/$USER"
+  DOCKER_USER="-u $(id -u $USER):$(id -g $USER)"
+  DOCKER_OPTS="-it"
+fi
+
+docker run ${DOCKER_OPTS} \
+           --gpus all \
+           -w ${EXEC_PATH} \
+           --shm-size=2gb \
+           -h bn_docker \
+           -v /etc/group:/etc/group:ro \
+           -v /etc/passwd:/etc/passwd:ro \
+           -v ${USER_DIR}:${USER_DIR} \
+           -v /mnt:/mnt \
+           ${DOCKER_USER} \
+           bottlenome/bracket_net:latest ${COMMAND}
