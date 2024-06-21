6deceec4179215a5f21e8603b2f2cabce6371a0f
bottlenome <bottlenome@gmail.com>
2024-06-21 21:44:31 +0900

    Refactor planning model
    Move Stacked UNet to generic model

--- bracket_net/domain/planning/up_causal_unet.py
+++ bracket_net/domain/planning/up_causal_unet.py
@@ -7,42 +7,13 @@ from ...model import up_causal_unet
 from .util import CommonModule
 
 
-class StackedUnet(nn.Module):
-    def __init__(self, config, max_len=4100):
-        super().__init__()
-        self.embed = nn.Embedding(config.gpt.d_vocab + 1, config.gpt.d_model)
-        self.max_len = max_len
-        self.unets = nn.ModuleList()
-        for i in range(config.gpt.num_layers):
-            self.unets.add_module(
-                f'up_causal_unet_{i}',
-                up_causal_unet.UpCasualUnet(config.gpt.d_model, max_len))
-        self.unembed = torch.nn.Linear(config.gpt.d_model, config.gpt.d_vocab + 1)
-
-    
-    def forward(self, x):
-        len = x.size(1)
-        x = self.embed(x)
-        # [batch, seq, dim] -> [batch, dim, seq]
-        x = x.permute(0, 2, 1)
-        for net in self.unets:
-            if len < self.max_len:
-                x = net(F.pad(x[:, :, :len], (0, self.max_len - len)))
-            else:
-                x = net(x[:, :, :self.max_len])
-        x = x[:, :, :self.max_len]
-        # [batch, dim, seq] -> [batch, seq, dim]
-        x = x.permute(0, 2, 1)
-        x = self.unembed(x)
-        # [batch, seq, dim] -> [batch, dim, seq]
-        x = x.permute(0, 2, 1)
-        return x
-
-
 class Naive(CommonModule):
     def __init__(self, config, max_len=4100):
         super().__init__(config)
-        self.model = StackedUnet(config, max_len=max_len)
+        self.model = up_causal_unet.StackedUnet(config.gpt.d_vocab,
+                                                config.gpt.d_model,
+                                                config.gpt.num_layers,
+                                                max_len=max_len)
         self.max_len = max_len
         self.d_vocab = config.gpt.d_vocab + 1
 
@@ -72,13 +43,26 @@ class Naive(CommonModule):
 
 
 if __name__ == '__main__':
-    class Config:
+    class GPT:
         d_vocab = 6
         d_model = 128
         num_layers = 4
+    class Params:
+        lr = 0.001
+        batch_size = 10
+        enable_entropy_loss = False
+    class Model:
+        type = "1d"
+    class Config:
+        gpt = GPT()
+        params = Params()
+        model = Model()
     config = Config()
     max_len=4100
     model = Naive(config, max_len=max_len)
-    x = torch.randint(0, 6, (100, max_len))
-    y = model(x)
+    map_designs = torch.randint(0, 6, (10, 1, 32, 32))
+    start_maps = torch.randint(0, 6, (10, 1, 32, 32))
+    goal_maps = torch.randint(0, 6, (10, 1, 32, 32))
+    out_trajs = torch.randint(0, 6, (10, 1, 32, 32))
+    y = model(map_designs, start_maps, goal_maps, out_trajs)
     print(y.shape)
--- test/unittest.sh
+++ test/unittest.sh
@@ -3,5 +3,6 @@
 echo "Running unittests"
 
 python -m bracket_net.domain.planning.reformer
+python -m bracket_net.domain.planning.up_causal_unet
 
 echo "PASS"
